5d6009b9a5e40039e55f31545b0c9d1c9aeff046
KarolannMauger <karolannmauger@gmail.com>
2025-11-29 12:50:16 -0500

    feat: add users module, schema and init mongodb connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot(
+      process.env.MONGO_URI || 'mongodb://localhost:27017/rockets-hack25',
+    ),
+    UsersModule,
+  ],
 })
 export class AppModule {}
